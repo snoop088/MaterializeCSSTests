86264f36094bfe0f6526aea800ab367f73efa71f
Nikolay Dimitrov <snoop088@gmail.com>
2016-07-16 23:53:12 +0300

    Configured, CSS. JS, Modules. Clean MaterializeCSS starting prj.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import "angular2-materialize";
 
 if (environment.production) {
   enableProdMode();
--- src/system-config.ts
+++ src/system-config.ts
@@ -7,10 +7,22 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  "materialize": "vendor/materialize-css",
+  "angular2-materialize": "vendor/angular2-materialize",
+  "jquery": "vendor/jquery"
 };
 
 /** User packages configuration. */
 const packages: any = {
+  'materialize': {
+    "format": "global",
+    "main": "dist/js/materialize",
+    "defaultExtension": "js"
+  },
+  'angular2-materialize': {
+    "main": "dist/index",
+    "defaultExtension": "js"
+  }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
